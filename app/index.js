ed0a202c0756617c330e32285c37d1369bfc4c91
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2013-09-16 13:14:27 -0400

    removed coloring

--- app/index.js
+++ app/index.js
@@ -10,9 +10,9 @@ var AppGenerator = module.exports = function Appgenerator(args, options, config)
 
     this.on('end', function () {
         if (options['skip-install']) {
-            console.log('\n\nI\'m all done. Just run ' + 'npm install & bower install --dev'.bold.yellow + ' to install the required dependencies.\n\n');
+            console.log('\n\nI\'m all done. Just run ' + 'npm install & bower install --dev' + ' to install the required dependencies.\n\n');
         } else {
-            console.log('\n\nI\'m all done. Running ' + 'npm install & bower install'.bold.yellow + ' for you to install the required dependencies. If this fails, try running the command yourself.\n\n');
+            console.log('\n\nI\'m all done. Running ' + 'npm install & bower install' + ' for you to install the required dependencies. If this fails, try running the command yourself.\n\n');
             spawn(win32 ? 'cmd' : 'npm', [win32 ? '/c npm install' : 'install'], { stdio: 'inherit' });
             spawn(win32 ? 'cmd' : 'bower', [win32 ? '/c bower install' : 'install'], { stdio: 'inherit' });
             spawn(win32 ? 'cmd' : 'composer', [win32 ? '/c composer install' : 'install'], { stdio: 'inherit' });
